97d39d9a6e4f78d027e107933e397454184ae67b
Tui Baraniuk <tuialexandre@gmail.com>
2021-02-03 13:04:37 -0300

    Readded param node handler

--- src/sonar_to_scan.cpp
+++ src/sonar_to_scan.cpp
@@ -6,18 +6,19 @@ int main(int argc, char** argv)
 {
 	ROS_INFO("Starting sonar to scan node");
 	ros::init(argc, argv, "sonar_to_scan");
-	ros::NodeHandle nh;
+	ros::NodeHandle nh, nhp("~");
 	std::vector<std::string> sensors_topics;
 	std::vector<std::string> sensors_frames;
 
-	nh.getParam("sonar_topics", sensors_topics);
-	nh.getParam("sonar_frames", sensors_frames);
-	SonarManager teste(&nh, nh.param<std::string>("laserScan/input", "scan"),
-                                nh.param<std::string>("laserScan/output", "scan/ultrasound"));
+	nhp.getParam("sonar_topics", sensors_topics);
+	nhp.getParam("sonar_frames", sensors_frames);
+	SonarManager teste(&nh, nhp.param<std::string>("laserScan/input", "scan"),
+                                nhp.param<std::string>("laserScan/output", "scan/ultrasound"));
 
 	for(int i=0; i<sensors_topics.size(); i++){
 		teste.addSonar(&nh, sensors_topics[i], sensors_frames[i]);
 	}
+	teste.printSensors();
 
 	ros::spin();
 	return 0;
